ff7325b280b56f2037088987b78620a07da2600d
Anton Sviridov <keynmol@gmail.com>
2022-09-16 17:11:47 +0100

    Use LangoustineApp

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,34 +1,8 @@
 package grammarsy
 
-import cats.instances.future.*
-
 import scala.concurrent.ExecutionContext.Implicits.global
-import cats.effect.IO
-import cats.effect.IOApp
+import langoustine.lsp.app.LangoustineApp
+import scala.concurrent.Future
 
-import jsonrpclib.fs2.{*, given}
-
-object GrammarJSServer extends IOApp.Simple:
-  def run =
-    FS2Channel[IO](2048, None)
-      .evalTap(server.bind)
-      .flatMap(channel =>
-        fs2.Stream
-          .eval(IO.never) // running the server forever
-          .concurrently(
-            fs2.io
-              .stdin[IO]
-              .through(lsp.decodePayloads[IO])
-              .through(channel.input)
-          )
-          .concurrently(
-            channel.output
-              .through(lsp.encodePayloads[IO])
-              .through(fs2.io.stdout[IO])
-          )
-      )
-      .compile
-      .drain
-      .guarantee(IO.consoleForIO.errorln("Terminating server"))
-  end run
-end GrammarJSServer
+object GrammarJSServer extends LangoustineApp.FromFuture.Simple:
+  def server = Future { grammarsy.server }
